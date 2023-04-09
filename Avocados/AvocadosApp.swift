a002729a65c7003c24b64375948f5cf8aa26cb40
Dzmitry Bladyka <bladykodmitry@gmail.com>
2023-04-09 19:43:28 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Dzmitry Bladyka on 9.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
